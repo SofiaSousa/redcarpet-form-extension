97825033e206ba09631a0e05f5aecba5c9133e4d
Sofia Sousa <csofiamsousa@gmail.com>
2022-06-14 13:04:42 +0100

    v0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
+### v0.2.1 - 2022-06-14
 ### v0.2.0 - 2022-06-14
 - feat: Change single input pattern
--- lib/redcarpet/form/extension/version.rb
+++ lib/redcarpet/form/extension/version.rb
@@ -3,7 +3,7 @@
 module Redcarpet
   module Form
     module Extension
-      VERSION = '0.2.0'
+      VERSION = '0.2.1'
     end
   end
 end
